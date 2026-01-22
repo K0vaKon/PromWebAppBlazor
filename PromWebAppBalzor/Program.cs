538a857ee732b48746911a24fc2d9c19ac576cc5
Cactus <Cactus@DESKTOP-JUSEBHS>
2026-01-22 10:31:04 +0100

    DB

--- PromWebAppBalzor/Program.cs
+++ PromWebAppBalzor/Program.cs
@@ -2,21 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PromWebAppBalzor;
 
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Регистрируем ОДИН HttpClient, который смотрит на API друга
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("http://localhost:7188/") // Убедись, что у друга именно http, а не https
+});
 
+// ЗАПУСК должен быть в самом конце
 await builder.Build().RunAsync();
-
-
-//connectie Front-end / Back-end (API)
-builder.Services.AddScoped(sp =>
-    new HttpClient
-    {
-        BaseAddress = new Uri("http://localhost:7188/") //poort API
-    });
-
